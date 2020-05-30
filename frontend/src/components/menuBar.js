4f9b3177b4bca0cf3284ca9b6632f70b5403988f
Nunoobjr <obertran.lima@fasminvestimentos.com.br>
2020-05-30 18:31:24 -0300

    menuBar

--- frontend/src/components/menuBar.js
+++ frontend/src/components/menuBar.js
@@ -1,5 +1,40 @@
 import React from 'react'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import "./styles.css"
 
 export default function menuBar(){
-     return <div></div>
+     return (
+         <div>
+             <ul className="listaCat">
+                <li id="inicio" className="liCFilho"><a>Inicio</a></li>
+                <li className="liCFilho"><a>Jogos<ArrowDropDownIcon/></a>
+                    <ul className="listaCat2">
+                        <li><p>Uncharted</p></li>
+                        <li><p>Mario Kart</p></li>
+                    </ul>
+                </li>
+                <li className="liCFilho"><a>Mangas<ArrowDropDownIcon/></a>
+                    <ul className="listaCat2">
+                        <li><p>Dragon Ball Z</p></li>
+                    </ul>
+                </li>
+                <li className="liCFilho"><a>Animes<ArrowDropDownIcon/></a>
+                    <ul className="listaCat2">
+                        <li><p>Dragon Ball Z</p></li>
+                        <li><p>the rising of the shield hero of the mommy</p></li>
+                    </ul>
+                </li>
+                <li className="liCFilho"><a>Filmes<ArrowDropDownIcon/></a>
+                    <ul className="listaCat2">
+                        <li><p>Poderoso Chefão</p></li>
+                    </ul>
+                </li>
+                <li className="liCFilho"><a>Series<ArrowDropDownIcon/></a>
+                    <ul className="listaCat2">
+                        <li><p>13 reasons why</p></li>
+                    </ul>
+                </li>
+            </ul>
+         </div>
+     )
 }
