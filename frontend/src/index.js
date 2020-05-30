794c32408f8310c705b38258af5e92bf25f5b0fd
Nunoobjr <obertran.lima@fasminvestimentos.com.br>
2020-05-30 17:10:08 -0300

    routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import Routes from './routes';
 
 
 ReactDOM.render(
 
-  <App />,
+  <Routes />,
 
   document.getElementById('root')
 );
