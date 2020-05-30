8bb28b765c1c672c11bc88aeeed09d7e0030fb7e
Andrey Rezende <Andrey Rezende>
2020-05-30 17:14:58 -0300

    origin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-
+import Banner from './Banner'
 function App() {
   return (
     <h1>Hello World</h1> 
---
+++ frontend/src/components/banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.cs';
+
+export default function Banner() {
+    return (
+        <div className="container">
+            
+        </div>
+    );
+}
