a320ea76215e0b5ea0664b00f8083124023faa65
Andrey Rezende <Andrey Rezende>
2020-05-31 00:59:07 -0300

    2° merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
 import Banner from "./components/banner"
 
-=======
 import MenuBar from "./components/menuBar"
 import CatPosts from "./components/catPosts"
->>>>>>> OB
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
         <Banner/>
-=======
         <MenuBar/>
         <div style={{marginTop:"5%"}}><CatPosts/></div>
         <div style={{marginTop:"5%"}}><h2>Reviews Recentes:</h2></div>
->>>>>>> OB
     </div>
   );
 }
